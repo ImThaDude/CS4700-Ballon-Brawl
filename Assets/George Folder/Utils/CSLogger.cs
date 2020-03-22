785a67db5d27dbf55df079a8efb29f457bf5fd12
George Quan <gpquanli@gmail.com>
2020-03-22 13:31:00 -0700

    Bad commit.

---
+++ Assets/George Folder/Utils/CSLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSLogger
+{
+    
+    public static void Log(object o) {
+        Debug.Log(o.ToString());
+    }
+    
+    public static void L(object o) {
+        Log(o);
+    }
+
+}
