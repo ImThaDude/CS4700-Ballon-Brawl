c1efeef0f18491923ee5378d77689e917359d874
Autofire <Autofire@users.noreply.github.com>
2020-03-24 11:53:45 -0700

    Now can hold down jump.

--- Assets/Scripts/BalloonFighterBody.cs
+++ Assets/Scripts/BalloonFighterBody.cs
@@ -16,20 +16,34 @@ public class BalloonFighterBody : MonoBehaviour
 	public Rigidbody2D rb;
 	public Animator anim;
 
-	[Space(10)]
-	[Tooltip("Used when jump is hit while airborne.")]
-	public float flapImpulse = 10f;
+	[Header("Jump tuning")]
 	[Tooltip("Used when jump is hit while grounded.")]
 	public float jumpImpulse = 4f;
+	[Tooltip("If we are holding jump, this is number of seconds " +
+	         "before autoflap begins.")]
+	public float jumpToAutoFlapDelay = 1.0f;
+
+	[Header("Flap tuning")]
+	[Tooltip("Used when jump is hit while airborne.")]
+	public float flapImpulse = 10f;
+	[Tooltip("Time between flaps (in seconds) while jump is held.")]
+	[Range(0f, 2f)] public float autoFlapPeriod = 0.5f;
+	[Tooltip("This controls how much the player can influence the " +
+	         "left/right movement. Higher numbers makes it feel " +
+			 "more stiff, but low numbers make it so that the " +
+			 "player will fall more when trying to move and flap.")]
+	[Range(0.01f, 5f)] public float upwardFlapBias = 1f;
+
+	[Header("Standing tuning")]
 	[Tooltip("Maximum speed while on the ground.")]
 	public float groundMovementSpeed = 4f;
-	
 	[Space(10)]
 	public float groundCastLength = 0.5f;
 	public LayerMask groundMask;
 
 	private float moveAmount;
 	private bool isJumping;
+	private float timeOfNextAutoflap = 0f;
 	
 	public bool IsGrounded {
 		get {
@@ -40,17 +54,6 @@ public class BalloonFighterBody : MonoBehaviour
 				groundMask
 			);
 		}
-	}
-
-	/// <summary>
-	/// Move the character horizontally. Note that if we are in the
-	/// air and not flapping, we won't move at all.
-	///
-	/// This should get called every frame.
-	/// </summary>
-	/// <param name="context"></param>
-	public void MoveHorizontal(InputAction.CallbackContext context) {
-		MoveHorizontal(context.ReadValue<float>());
 	}
 
 	/// <summary>
@@ -69,18 +72,18 @@ public class BalloonFighterBody : MonoBehaviour
 	/// <summary>
 	/// Jumps/flaps, gaining altitude.
 	/// </summary>
-	/// <param name="context"></param>
-	public void Jump(InputAction.CallbackContext context) {
-		if(context.performed) {
-			Jump();
-		}
-	}
+	/// <param name="nowJumping">
+	/// If true, we are going to be jumping/flapping. When false,
+	/// we're done gaining altitude.
+	/// </param>
+	public void SetJump(bool nowJumping = true) {
+		isJumping = nowJumping;
 
-	/// <summary>
-	/// Jumps/flaps, gaining altitude.
-	/// </summary>
-	public void Jump() {
-		isJumping = true;
+		if(isJumping) {
+			// If we're in the air, we want to immediately flap.
+			// Thus, the next (auto) flap will happen immediately.
+			timeOfNextAutoflap = Time.time;
+		}
 	}
 
 	private void Awake() {
@@ -96,26 +99,51 @@ public class BalloonFighterBody : MonoBehaviour
 	{
 		//Vector2 dir = new Vector2(moveAmount, 0);
 
+		/*
 		if(IsGrounded) {
 
-			if(isJumping) {
+			if(isJumping && Time.time >= timeOfNextAutoflap) {
 				rb.AddForce(Vector2.up * jumpImpulse);
+
+				timeOfNextAutoflap = Time.time + jumpToAutoFlapDelay;
 			}
 			else {
-				//rb.velocity = dir * groundMovementSpeed;
 				rb.velocity = new Vector2(
-					//dir.x * groundMovementSpeed,
 					moveAmount * groundMovementSpeed,
 					rb.velocity.y
 				);
-
 			}
 		}
-		else if(isJumping) {
+		else if(isJumping && Time.time >= timeOfNextAutoflap) {
+
 			Vector2 dir = new Vector2(moveAmount, 1).normalized;
 			rb.AddForce(dir * flapImpulse);
 
 			anim.SetTrigger("Flap");
+
+			timeOfNextAutoflap = Time.time + autoFlapPeriod;
+		}
+		*/
+		if(isJumping && Time.time >= timeOfNextAutoflap) {
+
+			if(IsGrounded) {
+				rb.AddForce(Vector2.up * jumpImpulse);
+				timeOfNextAutoflap = Time.time + jumpToAutoFlapDelay;
+			}
+			else {
+				Vector2 dir = new Vector2(
+					moveAmount, upwardFlapBias).normalized;
+				rb.AddForce(dir * flapImpulse);
+				timeOfNextAutoflap = Time.time + autoFlapPeriod;
+
+				anim.SetTrigger("Flap");
+			}
+		}
+		else if(IsGrounded) {
+			rb.velocity = new Vector2(
+				moveAmount * groundMovementSpeed,
+				rb.velocity.y
+			);
 		}
 
 		anim.SetFloat("Movement", Mathf.Abs(moveAmount));
@@ -130,6 +158,6 @@ public class BalloonFighterBody : MonoBehaviour
 	/// </summary>
 	private void ResetControlVars() {
 		//dir = Vector2.zero;
-		isJumping = false;
+		//isJumping = false;
 	}
 }
---
+++ Assets/Scripts/BalloonFighterPlayerBrain.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// This is the interface between the PlayerInput component and
+/// the balloon fighter body.
+/// </summary>
+public class BalloonFighterPlayerBrain : MonoBehaviour
+{
+	public BalloonFighterBody body;
+
+	private void Awake() {
+		Assert.IsNotNull(body);
+	}
+
+	/// <summary>
+	/// Move the character horizontally. Note that if we are in the
+	/// air and not flapping, we won't move at all.
+	///
+	/// This should get called every frame.
+	/// </summary>
+	/// <param name="context"></param>
+	public void MoveHorizontal(InputAction.CallbackContext context) {
+		body.MoveHorizontal(context.ReadValue<float>());
+	}
+
+	/// <summary>
+	/// Jumps/flaps, gaining altitude.
+	/// </summary>
+	/// <param name="context"></param>
+	public void Jump(InputAction.CallbackContext context) {
+		if(context.performed) {
+			body.SetJump(true);
+		}
+		else if(context.canceled) {
+			body.SetJump(false);
+		}
+	}
+}
