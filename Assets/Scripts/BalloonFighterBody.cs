549d5ca4aa694a04a5454c3128e5e5c2fc81e30c
George Quan <gpquanli@gmail.com>
2020-03-24 12:44:23 -0700

    Finalized addition. Added start button to be binded to enter in keyboard1 for testing.

--- Assets/Scripts/BalloonFighterBody.cs
+++ Assets/Scripts/BalloonFighterBody.cs
@@ -13,152 +13,171 @@ using UnityEngine.InputSystem;
 /// </summary>
 public class BalloonFighterBody : MonoBehaviour
 {
-	public Rigidbody2D rb;
-	public Animator anim;
+    public Rigidbody2D rb;
+    public Animator anim;
 
-	[Header("Jump tuning")]
-	[Tooltip("Used when jump is hit while grounded.")]
-	public float jumpImpulse = 4f;
-	[Tooltip("If we are holding jump, this is number of seconds " +
-	         "before autoflap begins.")]
-	public float jumpToAutoFlapDelay = 1.0f;
+    [Header("Jump tuning")]
+    [Tooltip("Used when jump is hit while grounded.")]
+    public float jumpImpulse = 4f;
+    [Tooltip("If we are holding jump, this is number of seconds " +
+             "before autoflap begins.")]
+    public float jumpToAutoFlapDelay = 1.0f;
 
-	[Header("Flap tuning")]
-	[Tooltip("Used when jump is hit while airborne.")]
-	public float flapImpulse = 10f;
-	[Tooltip("Time between flaps (in seconds) while jump is held.")]
-	[Range(0f, 2f)] public float autoFlapPeriod = 0.5f;
-	[Tooltip("This controls how much the player can influence the " +
-	         "left/right movement. Higher numbers makes it feel " +
-			 "more stiff, but low numbers make it so that the " +
-			 "player will fall more when trying to move and flap.")]
-	[Range(0.01f, 5f)] public float upwardFlapBias = 1f;
+    [Header("Flap tuning")]
+    [Tooltip("Used when jump is hit while airborne.")]
+    public float flapImpulse = 10f;
+    [Tooltip("Time between flaps (in seconds) while jump is held.")]
+    [Range(0f, 2f)] public float autoFlapPeriod = 0.5f;
+    [Tooltip("This controls how much the player can influence the " +
+             "left/right movement. Higher numbers makes it feel " +
+             "more stiff, but low numbers make it so that the " +
+             "player will fall more when trying to move and flap.")]
+    [Range(0.01f, 5f)] public float upwardFlapBias = 1f;
 
-	[Header("Standing tuning")]
-	[Tooltip("Maximum speed while on the ground.")]
-	public float groundMovementSpeed = 4f;
-	[Space(10)]
-	public float groundCastLength = 0.5f;
-	public LayerMask groundMask;
+    [Header("Standing tuning")]
+    [Tooltip("Maximum speed while on the ground.")]
+    public float groundMovementSpeed = 4f;
+    [Space(10)]
+    public float groundCastLength = 0.5f;
+    public LayerMask groundMask;
 
-	private float moveAmount;
-	private bool isJumping;
-	private float timeOfNextAutoflap = 0f;
+    private float moveAmount;
+    private bool isJumping;
+    private float timeOfNextAutoflap = 0f;
 
-	//George invasion variables.........
-	private bool canFly = true;
-	public bool isFloating = false;
-	public float floatVelocity = 2f;
-	public float floatClampLerp = 0.1f;
-	public bool hasFainted = false;
-	public bool isIdle = false;
-	public AudioClip jumpAudioClip;
-	//-----------------------------------
-	
-	public bool IsGrounded {
-		get {
-			return Physics2D.Raycast(
-				rb.transform.position,
-				Vector2.down,
-				groundCastLength,
-				groundMask
-			);
-		}
-	}
+    //George invasion variables.........
+    private bool canFly = true;
+    public bool isFloating = false;
+    public float floatVelocity = 2f;
+    public float floatClampLerp = 0.1f;
+    public bool hasFainted = false;
+    public bool isIdle = false;
+    public AudioClip jumpAudioClip;
+    //-----------------------------------
 
-	/// <summary>
-	/// Move the character horizontally. Note that if we are in the
-	/// air and not flapping, we won't move at all.
-	/// 
-	/// This should get called every frame.
-	/// </summary>
-	/// <param name="direction">
-	/// -1 is fastest to the left, and 1 is fastest to the right.
-	/// </param>
-	public void MoveHorizontal(float direction) {
-		moveAmount = direction;
-	}
+    public bool IsGrounded
+    {
+        get
+        {
+            return Physics2D.Raycast(
+                rb.transform.position,
+                Vector2.down,
+                groundCastLength,
+                groundMask
+            );
+        }
+    }
 
-	/// <summary>
-	/// Jumps/flaps, gaining altitude.
-	/// </summary>
-	/// <param name="nowJumping">
-	/// If true, we are going to be jumping/flapping. When false,
-	/// we're done gaining altitude.
-	/// </param>
-	public void SetJump(bool nowJumping = true) {
-		isJumping = nowJumping;
+    /// <summary>
+    /// Move the character horizontally. Note that if we are in the
+    /// air and not flapping, we won't move at all.
+    /// 
+    /// This should get called every frame.
+    /// </summary>
+    /// <param name="direction">
+    /// -1 is fastest to the left, and 1 is fastest to the right.
+    /// </param>
+    public void MoveHorizontal(float direction)
+    {
+        moveAmount = direction;
+    }
 
-		if(isJumping) {
-			// If we're in the air, we want to immediately flap.
-			// Thus, the next (auto) flap will happen immediately.
-			timeOfNextAutoflap = Time.time;
-		}
-	}
+    /// <summary>
+    /// Jumps/flaps, gaining altitude.
+    /// </summary>
+    /// <param name="nowJumping">
+    /// If true, we are going to be jumping/flapping. When false,
+    /// we're done gaining altitude.
+    /// </param>
+    public void SetJump(bool nowJumping = true)
+    {
+        isJumping = nowJumping;
 
-	//George invation code...
-	public float faintImpulseJump = 100f;
+        if (isJumping)
+        {
+            // If we're in the air, we want to immediately flap.
+            // Thus, the next (auto) flap will happen immediately.
+            timeOfNextAutoflap = Time.time;
+        }
+    }
 
-	public void SetFly(bool canFly) {
-		this.canFly = canFly;
-	}
+    //George invation code...
+    public float faintImpulseJump = 100f;
 
-	public void SetFloat(bool isFloating) {
-		this.isFloating = isFloating;
-	}
+    public void SetFly(bool canFly)
+    {
+        this.canFly = canFly;
+    }
 
-	public void Drop() {
-		rb.velocity = Vector3.zero;
-		SetFly(false);
-	}
+    public void SetFloat(bool isFloating)
+    {
+        this.isFloating = isFloating;
+    }
 
-	public void Faint() {
-		SetFloat(false);
-		Drop();
-		rb.AddForce(Vector2.up * faintImpulseJump);
-		hasFainted = true;
-	}
-	//------------------------
+    public void Drop()
+    {
+        rb.velocity = Vector3.zero;
+        SetFly(false);
+    }
 
-	private void Awake() {
-		Assert.IsNotNull(rb);
-		Assert.IsNotNull(anim);
-	}
+    public void Faint()
+    {
+        SetFloat(false);
+        Drop();
+        rb.AddForce(Vector2.up * faintImpulseJump);
+        hasFainted = true;
+    }
+    //------------------------
 
-	private void OnEnable() {
-		ResetControlVars();
-	}
+    private void Awake()
+    {
+        Assert.IsNotNull(rb);
+        Assert.IsNotNull(anim);
+    }
 
-	private void Update()
-	{
-		//Vector2 dir = new Vector2(moveAmount, 0);
+    private void OnEnable()
+    {
+        ResetControlVars();
+    }
 
-		//More george invasion of the code
-		if (isFloating) {
-			Vector2 v = rb.velocity;
-			v.y = Mathf.Lerp (v.y, -floatVelocity, floatClampLerp);
-			rb.velocity = v;
-		}
+    private void Update()
+    {
+        //Vector2 dir = new Vector2(moveAmount, 0);
 
-		if (IsGrounded) {
-			isFloating = false;
+        //More george invasion of the code
+        if (isFloating)
+        {
+            Vector2 v = rb.velocity;
+            v.y = Mathf.Lerp(v.y, -floatVelocity, floatClampLerp);
+            rb.velocity = v;
+        }
 
-			if (hasFainted) {
-				//Destroy(gameObject);
-				hasFainted = false;
-			}
+        if (IsGrounded)
+        {
+            isFloating = false;
 
-			if (moveAmount == 0) {
-				isIdle = true;
-			} else {
-				isIdle = false;
-			}
-		} else {
-			isIdle = false;
-		}
-		//-------------------------------
+            if (hasFainted)
+            {
+                //Destroy(gameObject);
+                hasFainted = false;
+            }
 
-		/*
+            if (moveAmount == 0)
+            {
+                isIdle = true;
+            }
+            else
+            {
+                isIdle = false;
+            }
+        }
+        else
+        {
+            isIdle = false;
+        }
+        //-------------------------------
+
+        /*
 		if(IsGrounded) {
 
 			if(isJumping && Time.time >= timeOfNextAutoflap) {
@@ -183,46 +202,55 @@ public class BalloonFighterBody : MonoBehaviour
 			timeOfNextAutoflap = Time.time + autoFlapPeriod;
 		}
 		*/
-		if(isJumping && Time.time >= timeOfNextAutoflap) {
 
-			if(IsGrounded) {
-				rb.AddForce(Vector2.up * jumpImpulse);
-				timeOfNextAutoflap = Time.time + jumpToAutoFlapDelay;
 
-				//Some more George invasion.
-				AudioSource.PlayClipAtPoint(jumpAudioClip, transform.position);
-			}
-			else {
-				Vector2 dir = new Vector2(
-					moveAmount, upwardFlapBias).normalized;
-				rb.AddForce(dir * flapImpulse);
-				timeOfNextAutoflap = Time.time + autoFlapPeriod;
+        if (isJumping && Time.time >= timeOfNextAutoflap)
+        {
 
-				//Some more George invasion.
-				AudioSource.PlayClipAtPoint(jumpAudioClip, transform.position);
+            if (IsGrounded)
+            {
+                rb.AddForce(Vector2.up * jumpImpulse);
+                timeOfNextAutoflap = Time.time + jumpToAutoFlapDelay;
 
-				anim.SetTrigger("Flap");
-			}
-		}
-		else if(IsGrounded) {
-			rb.velocity = new Vector2(
-				moveAmount * groundMovementSpeed,
-				rb.velocity.y
-			);
-		}
+                //Some more George invasion.
+                AudioSource.PlayClipAtPoint(jumpAudioClip, transform.position);
+            }
+            //canFly is George invasion variable
+            else if (canFly)
+            {
 
-		anim.SetFloat("Movement", Mathf.Abs(moveAmount));
-		anim.SetBool("IsGrounded", IsGrounded);
-		anim.SetFloat("Dir", moveAmount);
+                Vector2 dir = new Vector2(
+                    moveAmount, upwardFlapBias).normalized;
+                rb.AddForce(dir * flapImpulse);
+                timeOfNextAutoflap = Time.time + autoFlapPeriod;
 
-		ResetControlVars();
-	}
+                //Some more George invasion.
+                AudioSource.PlayClipAtPoint(jumpAudioClip, transform.position);
 
-	/// <summary>
-	/// Reverts whatever control inputs were given.
-	/// </summary>
-	private void ResetControlVars() {
-		//dir = Vector2.zero;
-		//isJumping = false;
-	}
+                anim.SetTrigger("Flap");
+            }
+        }
+        else if (IsGrounded)
+        {
+            rb.velocity = new Vector2(
+                moveAmount * groundMovementSpeed,
+                rb.velocity.y
+            );
+        }
+
+        anim.SetFloat("Movement", Mathf.Abs(moveAmount));
+        anim.SetBool("IsGrounded", IsGrounded);
+        anim.SetFloat("Dir", moveAmount);
+
+        ResetControlVars();
+    }
+
+    /// <summary>
+    /// Reverts whatever control inputs were given.
+    /// </summary>
+    private void ResetControlVars()
+    {
+        //dir = Vector2.zero;
+        //isJumping = false;
+    }
 }
