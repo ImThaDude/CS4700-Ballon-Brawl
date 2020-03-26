daddd9790c7f09d0ed61d6f13d7f70665a132e6f
Autofire <Autofire@users.noreply.github.com>
2020-03-25 21:28:08 -0700

    Put in hack to allow more than one player on keyboard.

---
+++ Assets/Scripts/ForceAssignKeyboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class ForceAssignKeyboard : MonoBehaviour
+{
+	//public PlayerInput input;
+	//public string schemeName;
+
+	private void Awake() {
+		PlayerInput input = GetComponent<PlayerInput>();
+
+		if(input.defaultControlScheme.Contains("Keyboard")) {
+			Debug.Log("Using keyboard hack. Should be removed spawning players with code.");
+			input.SwitchCurrentControlScheme( input.defaultControlScheme, Keyboard.current );
+		}
+	}
+}
