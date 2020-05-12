30c3a482ee7b5af091b4ee98fa78a3c229905fe9
Autofire <Autofire@users.noreply.github.com>
2020-05-11 22:47:58 -0700

    The player now gets points over time.

---
+++ Assets/Scripts/BalloonTrip/GameManager/PlayerLifeTracker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerLifeTracker : MonoBehaviour
+{
+	[Tooltip("This should be the player in the scene, not a prefab.")]
+	public GameObject playerObject;
+	[Tooltip("Also disabled this component")]
+	public UnityEvent onPlayerDeath;
+
+	private void Update() {
+		if(playerObject == null) {
+			enabled = false;
+			onPlayerDeath.Invoke();
+		}
+	}
+}
---
+++ Assets/Scripts/BalloonTrip/GameManager/TimedPointGranter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedPointGranter : MonoBehaviour
+{
+	public float interval = 3f;
+	public int pointAmount = 5;
+
+	private float timeOfNextReward = 0f;
+
+    private void OnEnable()
+    {
+		timeOfNextReward = Time.time;
+    }
+
+    void Update()
+    {
+		while(Time.time > timeOfNextReward) {
+			timeOfNextReward += interval;
+			ScoreTracker.AddPoints(pointAmount);
+		}
+    }
+}
